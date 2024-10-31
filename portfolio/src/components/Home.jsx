5a61577144e239090554b371da90561ef8a8750c
Shruti <shruti24priya.com>
2024-10-31 13:29:41 +0530

    removed items Home

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -7,15 +7,15 @@ import hero from './Data/hero.json';
 const Home = () => {
   return (
     <div className="container home">
-        <div className="left">
-            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus sequi molestiae</h1>
-        <a href={pdf} download={"Resume"} className="btn btn-outline-warning">Download Resume</a>
-        </div>
-        <div className="right">
+    <div className="right">
         <div className="img">
             <img src= {"Assets/Images/hero/girlImage.jpg"} alt="hero" />
         </div>
-            
+      </div>
+    
+        <div className="left">
+            <h2>Hey!! welcome to my portfolio.So myself Shruti Priya,a passionate Frontend Developer dedicated to creating engaging and user-friendly web experiences. With a strong foundation in HTML, CSS, and JavaScript, I leverage modern frameworks like ReactJs </h2>
+        <a href={pdf} download={"Resume"} className="btn btn-outline-warning">Download Resume</a>
         </div>
     </div>
   )
